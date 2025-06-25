e98255f55661260cf8f625deab9141409694f710
Chun Man Wong <wroy123@gmail.com>
2025-06-25 13:12:04 +0900

    Fix onPhotoClick in Post component

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, Input } from '@angular/core';
 import { RouterModule, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
@@ -19,6 +19,8 @@ import { OverlayService } from '../../services/overlay.service';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  @Input() isMobile = false;
+  
   showUserMenu = false;
   isHumanArtTab = false;
   isFollowingOnly = false;
